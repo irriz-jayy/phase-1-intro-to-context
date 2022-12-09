2c336f17647ad5a8ad1fffc77bca77d7d5027945
irriz-jayy <jayfelix789@gmail.com>
2022-12-09 17:02:50 +0300

    Completed initialisation and creation of array with first function

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Your code here
+//Creates an array with the objects at the specified indices.
+function createEmployeeRecord(array) {
+  return {
+    firstName: array[0],
+    familyName: array[1],
+    title: array[2],
+    payPerHour: array[3],
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
